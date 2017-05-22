e77d398a451da2f57d2fddffcaaf8f5a45acb7f7
oktam <okta.maulana93@yahoo.com>
2017-05-22 17:32:27 +0700

    controler and view

---
+++ Sipp.Website/Areas/Organization/Controllers/InternalOrganizationController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Sipp.Website.Areas.Organization.Controllers
+{
+    public class InternalOrganizationController : Controller
+    {
+        // GET: Organization/InternalOrganization
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ Sipp.Website/Areas/Payment/Controllers/BillingController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Sipp.Website.Areas.Payment.Controllers
+{
+    public class BillingController : Controller
+    {
+        // GET: Payment/Billing
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult BillingList()
+        {
+            return View();
+        }
+    }
+}
